d49e50ccc65f8d1230ac04e4aa1924bb32cd7f5a
maksims.muizininks <maksims.muizininks@symate.com>
2023-09-11 17:01:24 +0200

    mock connection and response

--- src/main/scala/actors/ApiConsumerActor.scala
+++ src/main/scala/actors/ApiConsumerActor.scala
@@ -1,11 +1,44 @@
 package actors
 
+import actors.ApiConsumerActor.{BAD_RESPONSE, FetchUrl, FetchedResponse, getResponse}
 import akka.actor.{Actor, ActorLogging}
+import factories.{ConnectionFactory, IoFactory}
 
-class ApiConsumerActor extends Actor with ActorLogging{
+import java.net.HttpURLConnection
+import scala.util.Try
+
+class ApiConsumerActor extends Actor with ActorLogging {
   override def preStart(): Unit = {
     log.debug("Starting")
     super.preStart()
   }
-  override def receive: Receive = Actor.emptyBehavior
+
+  override def receive: Receive = {
+    case FetchUrl(url: String) =>
+      val conn = ConnectionFactory.connection(url)
+      val response: String = getResponse(conn).getOrElse(BAD_RESPONSE)
+
+      context.sender() ! FetchedResponse(response)
+  }
+}
+
+object ApiConsumerActor {
+  final val API_ENDPOINT = "https://api.open-meteo.com/v1/forecast?latitude=51.05&longitude=13.74&current_weather=true"
+  final val USER_AGENT = "Mozilla/5.0"
+  final val BAD_RESPONSE = "bad response :/"
+
+  case class FetchUrl(url: String)
+
+  case class FetchedResponse(data: String)
+
+  def getResponse(conn: HttpURLConnection): Option[String] = {
+    conn.setRequestMethod("GET")
+    conn.setRequestProperty("User-Agent", USER_AGENT)
+
+    conn.getResponseCode match {
+      case HttpURLConnection.HTTP_OK =>
+        Try(IoFactory.getSource(conn).getLines().mkString("|")).toOption
+      case _ => None
+    }
+  }
 }
--- src/test/scala/actors/ApiConsumerActorTest.scala
+++ src/test/scala/actors/ApiConsumerActorTest.scala
@@ -1,19 +1,57 @@
 package actors
 
+import actors.ApiConsumerActor.{BAD_RESPONSE, FetchUrl, FetchedResponse}
 import akka.actor.ActorSystem
-import akka.testkit.TestKit
+import akka.testkit.{ImplicitSender, TestActor, TestActorRef, TestKit}
+import factories.{ConnectionFactory, IoFactory}
+import org.mockito.ArgumentMatchers.any
 import org.mockito.MockitoSugar
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.wordspec.AnyWordSpecLike
+
+import java.net.HttpURLConnection
+import scala.io.Source
 
 
 class ApiConsumerActorTest extends TestKit(ActorSystem("ApiConsumerActorTest"))
   with AnyWordSpecLike
   with BeforeAndAfterAll
+  with ImplicitSender
   with MockitoSugar // when, expect, withObjectMocked, etc.
 {
   override protected def afterAll(): Unit =
     TestKit.shutdownActorSystem(system)
 
+  "api consumer" should {
+    "call factories and return a result" in {
+      val connMock = mock[HttpURLConnection]
+      when(connMock.getResponseCode).thenReturn(HttpURLConnection.HTTP_OK)
+
+      val responseString =
+        """{
+            "latitude":51.04,
+            "longitude":13.74,
+            "windspeed":5.1,
+            "winddirection":356,
+            "weathercode":1,
+            "is_day":1,
+            "time":"2023-09-11T11:00"
+          }""".stripMargin
+      val sourceMock = mock[Source]
+      when(sourceMock.getLines()).thenReturn(Iterator(responseString))
+
+      withObjectMocked[ConnectionFactory.type] {
+        withObjectMocked[IoFactory.type] {
+          when(ConnectionFactory.connection(any[String])).thenReturn(connMock)
+          when(IoFactory.getSource(connMock)).thenReturn(sourceMock)
+
+          val apiConsumer = TestActorRef[ApiConsumerActor]
+          apiConsumer ! FetchUrl(ApiConsumerActor.API_ENDPOINT)
+          val responseMessage = expectMsgType[FetchedResponse]
+          assert(responseMessage.data != BAD_RESPONSE)
+        }
+      }
+    }
+  }
 
 }
